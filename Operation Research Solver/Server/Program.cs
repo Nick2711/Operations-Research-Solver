070e61a75fe64be9bfaef4d4a50325fcf0abf590
Nick2711 <Nicholas2711kelly@gmail.com>
2025-08-04 19:09:25 +0200

    added base to work off of ,  just tring to get css stil te reference

---
+++ Operation Research Solver/Server/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure Kestrel to listen on specific ports
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000); // HTTP
+    options.ListenAnyIP(7239, listenOptions => listenOptions.UseHttps()); // HTTPS
+});
+
+// Services
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
